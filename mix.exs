37ada5f9daba348b74f14f72e2f2b0a369ba6fcc
René Föhring <rf@bamaru.de>
2016-12-08 22:18:09 +0100

    Bump version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Bunt.Mixfile do
   def project do
     [
       app: :bunt,
-      version: "0.1.6",
+      version: "0.2.0",
       elixir: "~> 1.1",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
